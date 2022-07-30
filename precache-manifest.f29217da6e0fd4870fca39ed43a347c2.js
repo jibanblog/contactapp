b0befa914abe58aacc755870e9b75fabee54c3a6
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2022-07-30 19:34:09 +0530

    Updates

---
+++ precache-manifest.f29217da6e0fd4870fca39ed43a347c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6c6cacd67190d9a7f2cf792020a239",
+    "url": "/contactapp/index.html"
+  },
+  {
+    "revision": "015435f00db4f2252734",
+    "url": "/contactapp/static/css/main.ccc5ce56.chunk.css"
+  },
+  {
+    "revision": "a4580bef63e2a4e8d6ae",
+    "url": "/contactapp/static/js/2.947b3946.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contactapp/static/js/2.947b3946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015435f00db4f2252734",
+    "url": "/contactapp/static/js/main.9e52df16.chunk.js"
+  },
+  {
+    "revision": "e4295b1f050e768d7496",
+    "url": "/contactapp/static/js/runtime-main.feed2d52.js"
+  }
+]);
